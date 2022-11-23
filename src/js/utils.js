d3eb538d7debda3a9820cbae7c8634f40b67432c
Peter DeVita <mewtwo2643@yahoo.com>
2022-11-22 19:12:46 -0500

    Clean up Git LFS loader

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,16 +9,19 @@ const hex2rgb = (hex) =>
 
 // Remove the first part which is an empty string, and the second which is
 // the project name
-let currentPath = window.location.pathname.split("/").slice(2, -1);
+let currentPath;
+if (!window.location.host.endsWith("github.io")) {
+    currentPath = window.location.pathname.split("/").slice(2, -1);
+}
+
 const gitLFS = (path) => {
     if (!window.location.host.endsWith("github.io")) {
         return path;
     }
     let newPath;
     if (path.startsWith("./")) {
-        let newPathParts = [...currentPath, ...path.split("/").splice(1, )];
+        let newPathParts = [...currentPath, ...path.split("/").splice(1)];
         newPath = "/" + newPathParts.join("/");
-        console.log(newPath);
     }
     return `https://media.githubusercontent.com/media/${GitHubPages.username}/${GitHubPages.project}/${GitHubPages.branch}${newPath}`;
 }
