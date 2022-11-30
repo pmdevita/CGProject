da9e92d0bbadfa68ec468b7d0bc739a4ea80b4fb
Peter DeVita <mewtwo2643@yahoo.com>
2022-11-30 13:50:31 -0500

    Add support for loading gltf, add port mackerel program

--- src/js/graphics/model-loader.js
+++ src/js/graphics/model-loader.js
@@ -2,8 +2,13 @@
 // https://observablehq.com/@spattana/model-loaders-using-threejs
 
 
+import {gitLFS} from "../utils.js";
+
 const ModelSupported = {
-    "obj": THREE.OBJLoader
+    "obj": THREE.OBJLoader,
+    "gltf": THREE.GLTFLoader,
+    "glb": THREE.GLTFLoader,
+    "collada": THREE.ColladaLoader
 }
 
 const loadObject = (url, loader, mtls) => {
@@ -31,10 +36,19 @@ const loadObject = (url, loader, mtls) => {
 
 
 const loadModelFromURL = async (url, modelFormat) => {
-    if (!modelFormat) return "Missing model format parameter";
-    const loader = ModelSupported[modelFormat.toLowerCase()];
+    let format;
+    if (modelFormat) {
+        format = modelFormat;
+    } else {
+        let parts = url.split(".");
+        format = parts[parts.length - 1];
+    }
+
+    if (!format || !ModelSupported[format]) return "Missing model format parameter";
+    const loader = ModelSupported[format.toLowerCase()];
     if (!loader) return "Model not supported";
-    let loadedModel = await loadObject(url, new loader());
+    let loadedModel = await loadObject(gitLFS(url), new loader());
+    console.log(loadedModel);
     return createSCs(loadedModel);
 }
 
@@ -42,116 +56,57 @@ const loadModelFromURL = async (url, modelFormat) => {
 const createSCs = (obj) => {
     const sceneGraph = {};
     let scs = [];
-    if (obj.scene) getNode(obj.scene, mat4.create());
-    else getNode(obj, mat4.create());
+    if (obj.scene) getNode(obj.scene, mat4.create()); else getNode(obj, mat4.create());
 
     function getNode(node, M) {
         const sc = {};
         sc.name = node.name;
 
-        const translation = node.position
-            ? [node.position.x, node.position.y, node.position.z]
-            : [0, 0, 0];
-        const quaternion = node.quaternion
-            ? [
-                node.quaternion.x,
-                node.quaternion.y,
-                node.quaternion.z,
-                node.quaternion.w
-            ]
-            : [0, 0, 0, 1];
+        const translation = node.position ? [node.position.x, node.position.y, node.position.z] : [0, 0, 0];
+        const quaternion = node.quaternion ? [node.quaternion.x, node.quaternion.y, node.quaternion.z, node.quaternion.w] : [0, 0, 0, 1];
         //const rotation = node.rotation?[node.rotation.x,node.rotation.y,node.rotation.z]:[0,0,0];// XYZ order
-        const scale =
-            node.scale && node.scale.x
-                ? [node.scale.x, node.scale.y, node.scale.z]
-                : [1, 1, 1];
+        const scale = node.scale && node.scale.x ? [node.scale.x, node.scale.y, node.scale.z] : [1, 1, 1];
 
-        sc.modelMatrix = mat4.multiply(
-            M,
-            mat4.fromRotationTranslationScale(quaternion, translation, scale)
-        );
+        sc.modelMatrix = mat4.multiply(M, mat4.fromRotationTranslationScale(quaternion, translation, scale));
 
         if (node.geometry || node.attributes) {
-            const attributes = node.geometry
-                ? node.geometry.attributes
-                : node.attributes;
-            if (
-                node.geometry &&
-                node.geometry.groups &&
-                node.geometry.groups.length > 0
-            ) {
-                const groups = node.geometry.groups;
-                const localScs = d3.range(0, groups.length, 1).map((i) => {
-                    /*
-                    return{
-                      positions : array2Darray(attributes.position.array.slice(groups[i].start*3, (groups[i].start+groups[i].count)*3),3),
-                      normals : array2Darray(attributes.normal.array.slice(groups[i].start*3, (groups[i].start+groups[i].count)*3),3),
-                      uvs : array2Darray(attributes.uv.array.slice(groups[i].start*2, (groups[i].start+groups[i].count)*2),2)
-                    }
-                    */
-                    return createSC(attributes, {
-                        start: groups[i].start,
-                        count: groups[i].count
-                    });
-                });
-                //return scs
-                localScs.forEach((d, i) => {
-                    //d.cells = d3.range(0,d.positions.length/3,1).map(i=>[i*3+0,i*3+1,i*3+2]);
-                    scs.push({ name: sc.name, sc: d, modelMatrix: sc.modelMatrix });
-                });
-            } else {
-                sc.sc = createSC(attributes);
-                scs.push(sc);
+            const attributes = node.geometry ? node.geometry.attributes : node.attributes;
+            sc.sc = createSC(attributes);
+            if (node.geometry.index) {
+                sc.sc.indices = node.geometry.index.array.slice();
             }
+            scs.push(sc);
         }
         if (node.children) node.children.forEach((d) => getNode(d, sc.modelMatrix));
     }
+
     return scs;
 }
 
 const createSC = (attributes, offset) => {
-    let positions = offset
-        ? attributes.position.array.slice(
-            offset.start * 3,
-            (offset.start + offset.count) * 3
-        )
-        : attributes.position.array.slice();
-    let normals = undefined,
-        uvs = undefined;
-    if (attributes.normal)
-        normals = offset
-            ? attributes.normal.array.slice(
-                offset.start * 3,
-                (offset.start + offset.count) * 3
-            )
-            : attributes.normal.array.slice();
-    else {
+    let newAttributes = {};
+    for (let key in attributes) {
+        newAttributes[key] = attributes[key].array.slice();
+    }
+    if (!attributes.normal) {
+        let positions = attributes.position.array.slice();
         let count = positions.length / 3;
         let Ns = [];
+        if (offset === undefined) {
+            offset = {start: 0, count: positions.length}
+        }
+
         for (let i = 0; i < offset.count; i += 3) {
             const k = offset.start + i;
-            const v0 = positions.slice(k * 9, k * 9 + 3),
-                v1 = positions.slice(k * 9 + 3, k * 9 + 6),
+            const v0 = positions.slice(k * 9, k * 9 + 3), v1 = positions.slice(k * 9 + 3, k * 9 + 6),
                 v2 = positions.slice(k * 9 + 6, k * 9 + 9);
-            const N = Array.from(
-                vec3.normalize(vec3.cross(vec3.subtract(v1, v0), vec3.subtract(v2, v0)))
-            );
+            const N = Array.from(v3.normalize(v3.cross(v3.subtract(v1, v0), v3.subtract(v2, v0))));
             Ns.push(N, N, N);
         }
-        normals = Ns.flat();
+        newAttributes.normal = Ns.flat();
     }
-    if (attributes.uv)
-        uvs = offset
-            ? attributes.uv.array.slice(
-                offset.start * 2,
-                (offset.start + offset.count) * 2
-            )
-            : attributes.uv.array.slice();
-    return {
-        positions,
-        normals,
-        uvs
-    };
+
+    return newAttributes;
 }
 
 
@@ -164,9 +119,9 @@ const computeModelExtent = function (o) {
 
 
     const extents = o.map((d) => {
-        const xExtent = d3.extent(d.sc.positions.filter((_, i) => i % 3 === 0));
-        const yExtent = d3.extent(d.sc.positions.filter((_, i) => i % 3 === 1));
-        const zExtent = d3.extent(d.sc.positions.filter((_, i) => i % 3 === 2));
+        const xExtent = d3.extent(d.sc.position.filter((_, i) => i % 3 === 0));
+        const yExtent = d3.extent(d.sc.position.filter((_, i) => i % 3 === 1));
+        const zExtent = d3.extent(d.sc.position.filter((_, i) => i % 3 === 2));
         return {
             min: [xExtent[0], yExtent[0], zExtent[0]],
             max: [xExtent[1], yExtent[1], zExtent[1]]
